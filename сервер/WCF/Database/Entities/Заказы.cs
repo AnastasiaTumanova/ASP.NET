678a063325417025670ddb3f27094daf45f10b5f
Anastasia Tumanova <100483000+AnastasiaTumanova@users.noreply.github.com>
2022-05-07 13:57:33 +0400

    Add files via upload

---
+++ сервер/WCF/Database/Entities/Заказы.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF.Database.Entities
+{
+    public class Заказы
+    {
+        [Key] public int Id_заказа { get; set; }
+
+
+        [Column("id_заказчика")]
+        public int id_заказчика { get; set; }
+
+        [Column("Дата")]
+        public DateTime Дата { get; set; }
+        [IgnoreDataMember]
+        public virtual Заказчики Заказчики { get; set; }
+    }
+}
